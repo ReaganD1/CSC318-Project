5b3af64692a77ea3bf317b5362409eae564171c4
SajedAtwa <sajedatwa78@gmail.com>
2024-07-11 22:49:27 -0400

    commited code

--- server/config/database.js
+++ server/config/database.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 // Setup a connection pool using the promise-based API
 const pool = mysql.createPool({
     connectionLimit: 10, // The maximum number of connections to create at once
-    host: 'db-csc-336.cd68y0a605xf.us-east-2.rds.amazonaws.com',    // Your database host
-    user: 'adminsOnly',         // Your database username
-    password: 'blahblah4',         // Your database password
-    database: 'db_project', // Your database name
+    host: 'csc318project.c1s6u6eai311.us-east-1.rds.amazonaws.com',    // Your database host
+    user: 'admin',         // Your database username
+    password: 'project318',         // Your database password
+    database: 'project318', // Your database name
     port: 3306            // Your database port
 }).promise();  // Use promise() to get a promise-based interface
 
