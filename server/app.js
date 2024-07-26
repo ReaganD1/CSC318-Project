19fe2a7f411022c0afafca00a11fd97e1d9aad5b
ReaganD1 <rdhar99999@gmail.com>
2024-07-25 23:26:55 -0400

    fix

--- server/app.js
+++ server/app.js
@@ -33,5 +33,5 @@ app.use(errorHandler);
 app.listen(process.env.PORT || 3001, () => {
     console.log(`Server started on port ${process.env.PORT || 3001}`);
 });
-module.exports = app; // Export app for vercel
+
 // testing a lottttttt
